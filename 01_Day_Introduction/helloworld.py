9c56cf51a642bcb06264db008fa05073438c0f64
Egittog <89788780+Egittog@users.noreply.github.com>
2024-06-25 17:44:52 -0400

    my notes in helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -7,6 +7,14 @@ print(2 * 3)   # multiplication(*)
 print(3 / 2)   # division(/)
 print(3 ** 2)  # exponential(**)
 print(3 % 2)   # modulus(%)
+
+# 13 mod 5 = 3
+print(2%3) #output2. 3 doesnt fit into 2, so what remains of that first integer,2, is 2 left.
+print(2%2) ##output0
+print(4%2) ##output0
+print(2%4) ##output2
+print("prints out the remainder")
+
 print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
